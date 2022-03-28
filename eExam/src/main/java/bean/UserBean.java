3fd56745ef10ff8b6fc467a28e36637819c84fd7
Jeel6040 <jeel14kalariya@gmail.com>
2022-03-28 11:03:28 +0530

    first commit

---
+++ eExam/src/main/java/bean/UserBean.java
@@ -0,0 +1,63 @@
+package bean;
+
+public class UserBean {
+	
+	private int userId; 
+	private String email;
+	private String password;
+	private String name;
+	private int roleId; 
+	private String roleName;
+	private int contactNo;
+	private String gender;
+	
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getContactNo() {
+		return contactNo;
+	}
+	public void setContactNo(int contactNo) {
+		this.contactNo = contactNo;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public int getRoleId() {
+		return roleId;
+	}
+	public void setRoleId(int roleId) {
+		this.roleId = roleId;
+	}
+	public String getRoleName() {
+		return roleName;
+	}
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+
+}
